834db8f93e7939e9a96b49ab73c1b9e97b61d6bf
Seunghui98 <hithere1012@naver.com>
2022-06-30 00:18:44 +0900

    project init

---
+++ Back/doobug2/src/main/java/com/succe22/doobug2/Doobug2Application.java
@@ -0,0 +1,13 @@
+package com.succe22.doobug2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Doobug2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Doobug2Application.class, args);
+	}
+
+}
---
+++ Back/doobug2/src/test/java/com/succe22/doobug2/Doobug2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.succe22.doobug2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Doobug2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
