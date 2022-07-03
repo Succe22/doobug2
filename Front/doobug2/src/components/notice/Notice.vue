124a2a889c5f3a4b04c10e80932c6e89dff1abf1
Junghojin <hojinnjung@gmail.com>
2022-07-03 11:46:23 +0900

    디렉토리 생성 및 임시 파일 생성
    Signed-off-by: Junghojin <hojinnjung@gmail.com>

---
+++ Front/doobug2/src/components/notice/Notice.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Doobug2Main',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
